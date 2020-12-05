d8d7712d5a95b103a82ffd1ae66c87bcee617bd4
nguyentb <nguyentb1999vn@gmail.com>
2020-12-05 13:33:09 +0700

    Revert "update module 2"
    This reverts commit 0f2206b5c52905529cb5324128dfaa71a13adec1.

--- 2/2.php
+++ 2/2.php
@@ -18,7 +18,7 @@ $less->compileFile('less/2.less', 'css/2.css');
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <title>2</title>
+        <title>139</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="<?php echo $url_path ?>/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
