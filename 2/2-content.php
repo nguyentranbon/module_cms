79d909f45fe059d8ee009df1902f41c1fa9b4770
nguyentb <nguyentb1999vn@gmail.com>
2020-12-19 14:41:28 +0700

    update module 1-2

--- 2/2-content.php
+++ 2/2-content.php
@@ -12,5 +12,5 @@ $url_path = $url_host . $matches[1][0];
 $url_path = str_replace('\\', '/', $url_path);
 ?>
 <div class="type-2">
-    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15673.899915121354!2d106.7580641!3d10.8514325!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb3ff69197b10ec4f!2sThu%20Duc%20College%20of%20Technology!5e0!3m2!1sen!2s!4v1607148884193!5m2!1sen!2s" width="100%" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+    <div id="map"></div> 
 </div>
--- 2/2.php
+++ 2/2.php
@@ -23,16 +23,26 @@ $less->compileFile('less/2.less', 'css/2.css');
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="<?php echo $url_path ?>/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
         <link href="<?php echo $url_path ?>/css/2.css" rel="stylesheet" type="text/css" />
-        
         <?php
-        if (!class_exists('lessc')) {
-            include ('./libs/lessc.inc.php');
-        }
-        $less = new lessc;
-        $less->compileFile('less/2.less', 'css/2.css');
+            if (!class_exists('lessc')) {
+                include ('./libs/lessc.inc.php');
+            }
+            $less = new lessc;
+            $less->compileFile('less/2.less', 'css/2.css');
         ?>
     </head>
     <body >
         <?php include '../2/2-content.php'; ?>
+
+        <script>
+      var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 8
+        });
+      }
+    </script>
+    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?sensor=false&callback=initMap"></script>
     </body>
 </html>
